0480dd4ab2cce1790db840d095cc992e2b7309f1
DoomDuck <docdoomduck@protonmail.com>
2025-02-21 16:17:45 +0100

    feat(kfs): working multiscreen TUI

--- tui/src/lib.rs
+++ tui/src/lib.rs
@@ -3,9 +3,11 @@ use vga::{Char, Color};
 
 mod logger;
 mod multi_screen;
+mod text_buffer;
 
 pub use logger::Logger;
 pub use multi_screen::MultiScreen;
+pub use text_buffer::TextBuffer;
 
 pub struct Screen {
     pub chars: [[Char; Self::WIDTH]; Self::HEIGHT],
@@ -22,6 +24,10 @@ impl Screen {
             width: Self::WIDTH as u16,
             height: Self::HEIGHT as u16,
         }
+    }
+
+    pub fn clear(&mut self) {
+        self.chars = [[Char::new(0); Self::WIDTH]; Self::HEIGHT]
     }
 
     pub unsafe fn write_to_vga(&self) {
@@ -57,4 +63,16 @@ pub trait Widget {
     fn render(&self, screen: &mut Screen, area: Rectangle);
 
     fn update(&mut self, _event: Self::Event) {}
+}
+
+impl<E> Widget for &mut dyn Widget<Event = E> {
+    type Event = E;
+
+    fn render(&self, screen: &mut Screen, area: Rectangle) {
+        (self as &dyn Widget<Event = E>).render(screen, area);
+    }
+
+    fn update(&mut self, event: Self::Event) {
+        (self as &mut dyn Widget<Event = E>).update(event);
+    }
 }
